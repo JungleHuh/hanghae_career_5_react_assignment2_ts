e7aa78fbb2f9165134474282dcf6572fe367baf2
홍길동 <fofn0766@gmail.com>
2024-10-14 13:45:55 +0900

    ErrorPage Unit Test Completed

--- src/pages/error/components/__tests__/ErrorPage.spec.tsx
+++ src/pages/error/components/__tests__/ErrorPage.spec.tsx
@@ -1,16 +1,30 @@
 import { screen } from '@testing-library/react';
 import { vi } from 'vitest';
 
-import render from '@/utils/test/render';
+import customRender from '@/utils/test/render';
 import { ErrorPage } from '../ErrorPage';
 
-// 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행 (react-router-dom의 useNavigate 모킹)
+// react-router-dom 모듈 모킹
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  };
+});
 
-it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
-  // Arrange: ErrorPage 컴포넌트를 렌더링
-  const { user } = await render(<ErrorPage />);
+const mockNavigate = vi.fn();
 
-  // Act: "뒤로 이동" 버튼을 클릭
+describe('ErrorPage', () => {
+  it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
+    // Arrange: ErrorPage 컴포넌트를 렌더링
+    const { user } = await customRender(<ErrorPage />);
 
-  // Assert: navigate 함수가 -1 인자로 호출되었는지 확인
+    // Act: "뒤로 이동" 버튼을 클릭
+    const backButton = await screen.findByText('뒤로 이동');
+    await user.click(backButton);
+
+    // Assert: navigate 함수가 -1 인자로 호출되었는지 확인
+    expect(mockNavigate).toHaveBeenCalledWith(-1);
+  });
 });
